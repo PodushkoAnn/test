41272f2c909a2f77be833f07b4810ccd89fd5f97
PodushkoAnn <42144985+PodushkoAnn@users.noreply.github.com>
2018-08-24 19:55:25 +0300

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -6,13 +6,13 @@ public class MainClass {
 
     protected static final int CARS_COUNT = 4;
     protected static final CountDownLatch START = new CountDownLatch(CARS_COUNT + 1);
-    protected static CountDownLatch FINISH;
+    protected static CountDownLatch finish;
     protected static boolean win;
 
     public static void main(String[] args) throws InterruptedException {
 
         Race race = new Race(new Road(60), new Tunnel(), new Road(40));
-        FINISH = new CountDownLatch(CARS_COUNT * race.getStages().size());
+        finish = new CountDownLatch(CARS_COUNT * race.getStages().size());
         Car[] cars = new Car[CARS_COUNT];
 
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
@@ -30,7 +30,7 @@ public class MainClass {
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
         START.countDown();
 
-        while(FINISH.getCount() > 0) Thread.sleep(100);
+        while(finish.getCount() > 0) Thread.sleep(100);
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
 
     }
