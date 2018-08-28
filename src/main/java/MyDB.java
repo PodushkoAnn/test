01b991cb1ff2f3c253db695beea94db78b86c25d
PodushkoAnn <42144985+PodushkoAnn@users.noreply.github.com>
2018-08-28 13:47:21 +0300

    Update MyDB.java

--- src/main/java/MyDB.java
+++ src/main/java/MyDB.java
@@ -46,69 +46,61 @@ public class MyDB {
     }
 
     public void newEntry(String surname, int grade) throws SQLException {
-        connect();
+
         if(grade > 0 && grade <= 5) {
             String sql = String.format("INSERT into students (surname, grades) VALUES ('%s', '%d');", surname, grade);
             stmt.executeUpdate(sql);
         } else System.out.println("В нашем университете ставят оценки от 1 до 5");
-        disconnect();
+
     }
 
     public String readEntry(int id) throws SQLException {
-        connect();
-        String result = "Кваква";
+
+        String result = null;
         String sql = String.format("SELECT surname, grades FROM students where id='%d'", id);
         ResultSet rs = stmt.executeQuery(sql);
         if(rs.next()) {
             result = rs.getString(1);
         }
-        disconnect();
         return result;
     }
 
     public String readEntry(String surname) throws SQLException {
-        connect();
+
         String sql = String.format("SELECT surname, grades FROM students where surname='%s'", surname);
         ResultSet rs = stmt.executeQuery(sql);
 
         if(rs.next()) {
             String s = rs.getString(1);
-            System.out.println(rs.getString(1));
-            System.out.println(rs.getInt(2));
-            disconnect();
             return s;
         }
-        disconnect();
         return null;
     }
 
     public void deleteEntry(int id) throws SQLException {
-        connect();
-            String sql = String.format("DELETE from students where id ='%d';", id);
-            stmt.executeUpdate(sql);
-        disconnect();
+
+        String sql = String.format("DELETE from students where id ='%d';", id);
+        stmt.executeUpdate(sql);
+
     }
 
     public void readAll() throws SQLException {
-        connect();
+
         String sql = String.format("SELECT * from students");
         ResultSet rs = stmt.executeQuery(sql);
         while(rs.next()) {
             System.out.println(rs.getString(2) + " " + rs.getInt(3));
         }
-        disconnect();
     }
 
     public int getID(String surname) throws SQLException {
-        connect();
+
         String sql = String.format("SELECT id from students where surname='%s'", surname);
         ResultSet rs = stmt.executeQuery(sql);
         if(rs.next()) {
             int x = rs.getInt(1);
-            disconnect();
             return x;
         }
-        disconnect();
         return -1;
     }
 }
